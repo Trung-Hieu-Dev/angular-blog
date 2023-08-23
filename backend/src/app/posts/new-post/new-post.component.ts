47726f30d804b7449d019a0661665d81f8b1f31a
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-08-23 15:35:04 +0700

    Backend - Setting Automate Permalink

--- backend/src/app/posts/new-post/new-post.component.ts
+++ backend/src/app/posts/new-post/new-post.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+  styleUrls: ['./new-post.component.css'],
 })
 export class NewPostComponent {
+  permalink: string = '';
 
+  onTitleChange($event: any) {
+    const title = $event.target.value;
+    this.permalink = title
+      .trim()
+      .toLowerCase()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '');
+  }
 }
