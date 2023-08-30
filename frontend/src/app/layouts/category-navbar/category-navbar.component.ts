109ab8ad8592d7d3f6f8fb0575b9d65cd87b649b
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-08-30 14:15:28 +0700

    Fontend - Load Categories On Navbar From Firebase

--- frontend/src/app/layouts/category-navbar/category-navbar.component.ts
+++ frontend/src/app/layouts/category-navbar/category-navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-category-navbar',
   templateUrl: './category-navbar.component.html',
-  styleUrls: ['./category-navbar.component.css']
+  styleUrls: ['./category-navbar.component.css'],
 })
-export class CategoryNavbarComponent {
+export class CategoryNavbarComponent implements OnInit {
+  categoriesArray: Array<object>;
 
+  constructor(private categoryService: CategoriesService) {}
+
+  ngOnInit() {
+    this.categoryService.loadData().subscribe((value) => {
+      this.categoriesArray = value;
+      console.log(value);
+    });
+  }
 }
